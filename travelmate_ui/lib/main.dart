281adae79290815d4737936e2e0fe5b31a520d36
Gayal Balasooriya <baigayal01@gmail.com>
2021-05-15 23:38:28 +0530

    Structure setup

--- travelmate_ui/lib/main.dart
+++ travelmate_ui/lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:google_sign_in/google_sign_in.dart';
-import 'package:firebase_auth/firebase_auth.dart';
+import 'package:travelmate_ui/ui/welocome_home.dart';
 
 
 void main() async {
@@ -23,100 +22,4 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class HomePage extends StatefulWidget {
-  @override
-  _HomePageState createState() => _HomePageState();
-}
 
-class _HomePageState extends State<HomePage> {
-
-  String _imageUrl;
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        centerTitle: true,
-        title: Text("TravelMate"),
-      ),
-      body: Center(
-        child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: FlatButton(
-              child: Text("Google-Signin"),
-              onPressed: () => _gSigin(),
-                          color: Colors.red,
-            ),
-          ),
-
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: FlatButton(
-              child: Text("Signin with Email"),
-              onPressed: () {},
-              color: Colors.orange,
-            ),
-          ),
-          
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: FlatButton(
-              child: Text("Create Account"),
-              onPressed: () {},
-              color: Colors.purpleAccent,
-            ),
-          ),
-
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: FlatButton(
-              child: Text('Signout'),
-              onPressed: () => _logout(),
-              color: Colors.redAccent,
-            ),
-          ),
-
-          Image.network(_imageUrl == null || _imageUrl.isEmpty ? "https://picsum.photos/250?image=9" : _imageUrl)
-        ],
-      ),
-      )
-      
-    );
-  }
-
-  final FirebaseAuth _auth = FirebaseAuth.instance;
-  final GoogleSignIn _googleSignIn = GoogleSignIn();
-
-  Future<User> _gSigin() async {
-    GoogleSignInAccount googleSignInAccount = await _googleSignIn.signIn();
-    GoogleSignInAuthentication googleSignInAuthentication = await googleSignInAccount.authentication;
-
-    AuthCredential credential = GoogleAuthProvider.credential(
-        idToken: googleSignInAuthentication.idToken,
-        accessToken: googleSignInAuthentication.accessToken
-    );
-
-    UserCredential googleUserCredential = await _auth.signInWithCredential(credential);
-
-    User user = googleUserCredential.user;
-    print(user.displayName);
-    print(user.photoURL);
-
-    setState(() {
-      _imageUrl = user.photoURL;
-    });
-
-    return user;
-
-  }
-
-  _logout() {
-    setState(() {
-      _googleSignIn.signOut();
-      _imageUrl = null;
-    });
-  }
-}
--- travelmate_ui/lib/ui/welocome_home.dart
+++ travelmate_ui/lib/ui/welocome_home.dart
@@ -1,347 +1,101 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 
-// class FirstApp extends StatelessWidget {
-
-//   final String title;
-//   FirstApp({Key key, this.title}) : super(key: key);
-
-//   @override
-//   Widget build(BuildContext context) {
-//     // return Container(
-//     //   color: Colors.green,
-//     //   child: Row(
-//     //     mainAxisAlignment: MainAxisAlignment.center,
-//     //     children:<Widget> [
-//     //       Text(
-//     //         "Hi",
-//     //         textDirection: TextDirection.ltr,
-//     //         style: TextStyle(color: Colors.blue),
-//     //       ),
-
-//     //       Text(
-//     //         "Hi",
-//     //         textDirection: TextDirection.ltr,
-//     //         style: TextStyle(color: Colors.blue),
-//     //       ),
-
-//     //       Expanded(
-//     //         child: Text(
-//     //           "Hi",
-//     //           textDirection: TextDirection.ltr,
-//     //           style: TextStyle(color: Colors.blue),
-//     //         ),
-//     //       ),
-
-//     //       Container(
-//     //         color: Colors.deepOrange,
-//     //         alignment: Alignment.bottomLeft,
-//     //         child: Text(
-//     //           "Hi",
-//     //           textDirection: TextDirection.ltr,
-//     //           style: TextStyle(color: Colors.blue),
-//     //         ),
-
-//     //       )
-
-
-//     //     ],
-//     //   ),
-//     // );
-
-//     void _method() {
-//       print("Search");
-//     }
-
-//     // return Scaffold(
-//     //   appBar: AppBar(
-//     //     backgroundColor: Colors.green,
-//     //     title: Text(
-//     //       "Hi",
-//     //     ),
-//     //     actions: [
-//     //       IconButton(
-//     //         icon: Icon(Icons.send), 
-//     //         onPressed: ()=> {debugPrint("Tapped")}
-//     //       ),
-//     //       IconButton(
-//     //         icon: Icon(Icons.search), 
-//     //         onPressed: _method
-//     //       )
-//     //     ],
-//     //   ),
-
-//     //   body: Container(
-//     //     alignment: Alignment.center,
-//     //     child: Column(
-//     //       mainAxisAlignment: MainAxisAlignment.center,
-//     //       children: [
-//     //         Text(
-//     //           "Hi",
-//     //           style: TextStyle(fontSize: 20),
-//     //         ),
-
-//     //         InkWell(
-//     //           child: Text("Hiiiii"),
-//     //           onTap: () => {
-//     //             debugPrint("Button Tapped")
-//     //           }
-//     //         )
-//     //       ],
-//     //     ),
-//     //   ),
-//     //   backgroundColor: Colors.grey,
-
-//     //   bottomNavigationBar: BottomNavigationBar(
-//     //     items: [
-//     //       BottomNavigationBarItem(icon: Icon(Icons.add), title: Text("bn1")),
-//     //       BottomNavigationBarItem(icon: Icon(Icons.add_alarm), title: Text("bn1")),
-//     //       BottomNavigationBarItem(icon: Icon(Icons.apps), title: Text("bn1")),
-//     //     ], onTap: (int i) => debugPrint("tapped $i")
-//     //   ),
-
-//     //   floatingActionButton: FloatingActionButton(
-//     //     onPressed: null,
-//     //     backgroundColor: Colors.indigo,
-//     //     tooltip: "hey",
-//     //     child: Icon(Icons.missed_video_call),
-//     //   ),
-//     // );
-//     return new Scaffold(
-//       appBar: AppBar(
-//         title: Text(title),
-//       ),
-//       body: Center(
-//         child: CustomButton(),
-//       ),
-//     );
-    
-//   }
-
-// }
-
-// class CustomButton extends StatelessWidget {
-//   @override
-//   Widget build(BuildContext context) {
-//     return GestureDetector(
-//       onTap: () {
-//         final snackbar = new SnackBar(
-//           content: Text("SnackBar"), 
-//           backgroundColor: Theme.of(context).backgroundColor,
-//           duration: Duration(milliseconds: 1000),
-//         ); 
-        
-//         Scaffold.of(context).showSnackBar(snackbar);
-//       },
-
-
-//       child: Container(
-//         padding: new EdgeInsets.all(18.0),
-//         decoration: BoxDecoration(
-//           color: Theme.of(context).buttonColor,
-//           borderRadius: BorderRadius.circular(5.5)
-//         ),
-//         child: Text("First Button"),
-//       )
-//     );
-//   }
-// }
-
-// class FirstApp extends StatefulWidget {
-//   @override
-//   _FirstAppState createState() => _FirstAppState();
-// }
-
-// class _FirstAppState extends State<FirstApp> {
-
-//   int _moneyCounter = 0;
-//   void _rainMoney() {
-//     setState(() {
-//       _moneyCounter += 100;
-
-//     });
-//   }
-
-
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       appBar: AppBar(
-//         title: Text("appbar"),
-//         backgroundColor: Colors.lightGreen,
-//       ),
-
-//       body: Container(
-        
-//         child: Column(
-          
-//           children: [
-//             Center(
-//               child: Text("Get Rich",
-//                       style: TextStyle(fontSize: 50, color: Colors.grey),  
-//                     ),
-//             ),
-
-//             Expanded(
-//               child: Center(
-//                 child: Text(
-//                   "$_moneyCounter",
-//                   style: TextStyle(
-//                     fontSize: 55, color: _moneyCounter > 10000 ? Colors.lightGreen : Colors.red
-//                   ),
-//                 ),
-//               ),
-//             ),
-
-//             Expanded(
-//               child: Center(
-//                 child: FlatButton(
-//                   color: Colors.orange,
-//                   onPressed: _rainMoney,
-//                   child: Text(
-//                     "Click",
-//                     style: TextStyle(fontSize: 30),
-//                   ),
-//                 ),
-//               ),
-//             ) 
-            
-//           ],
-//         )
-//       ),
-//     );
-//   }
-// }
-
-
-class FirstApp extends StatefulWidget {
+class HomePage extends StatefulWidget {
   @override
-  _FirstAppState createState() => _FirstAppState();
+  _HomePageState createState() => _HomePageState();
 }
 
-class _FirstAppState extends State<FirstApp> {
+class _HomePageState extends State<HomePage> {
 
-  String _welcomeText = "";
-
-  final TextEditingController _userController = new TextEditingController();
-  final TextEditingController _passwordController = new TextEditingController();
-
-  void _erase() {
-    setState(() {
-      _userController.clear();
-      _passwordController.clear();
-    }); 
-  }
-
-  void _showWelcome() {
-    setState(() {
-      if(_userController.text.isNotEmpty && _passwordController.text.isNotEmpty) {
-        _welcomeText = _userController.text;
-      }
-      else {
-        _welcomeText = "No name";
-      }
-    });
-  }
+  String _imageUrl;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Hi"),
         centerTitle: true,
-        backgroundColor: Colors.blueAccent,
+        title: Text("TravelMate"),
       ),
-
-      backgroundColor: Colors.blueGrey,
-
-      body: Container(
-        alignment: Alignment.topCenter,
-        child: ListView(
-          children: [
-            Image.asset(
-              "images/097 face.png",
-              width: 90.0,
-              height: 90.0,
-              color: Colors.greenAccent,
+      body: Center(
+        child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: FlatButton(
+              child: Text("Google-Signin"),
+              onPressed: () => _gSigin(),
+                          color: Colors.red,
             ),
+          ),
 
-            Container(
-              height: 180.0,
-              width: 380.0,
-              color: Colors.white,
-              child: Column(
-                children: [
-                  TextField(
-                    controller: _userController,
-                    decoration: InputDecoration(
-                      hintText: 'Username',
-                      icon: Icon(Icons.person)
-                    ),
-                  ),
-
-                  TextField(
-                    controller: _passwordController,
-                    decoration: InputDecoration(
-                      hintText: 'Password',
-                      icon: Icon(Icons.lock)
-                    ),
-                    obscureText: true,  //Hide password text
-                  ),
-
-                  Padding(
-                    padding: EdgeInsets.all(10.5),
-                  ),
-                  Row(
-                    children: [ 
-                      Container(
-                        margin: const EdgeInsets.only(left: 38.0),
-                        child: RaisedButton(
-                          color: Colors.orange,
-                          onPressed: _showWelcome,
-                          child: Text(
-                            "Login",
-                            style: TextStyle(fontSize: 30),
-                          ),
-                        ),
-                      ),
-                      
-                      Container(
-                        margin: const EdgeInsets.only(left: 120.0),
-                        child: RaisedButton(
-                          color: Colors.orange,
-                          onPressed: _erase,
-                          child: Text(
-                            "Clear",
-                            style: TextStyle(fontSize: 30),
-                          ),
-                        ),
-                      ),  
-                    ],
-                  )
-                ],
-              )
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: FlatButton(
+              child: Text("Signin with Email"),
+              onPressed: () {},
+              color: Colors.orange,
             ),
+          ),
           
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Padding(
-                  padding: const EdgeInsets.all(30.0),
-                ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: FlatButton(
+              child: Text("Create Account"),
+              onPressed: () {},
+              color: Colors.purpleAccent,
+            ),
+          ),
 
-                Text("Welcome $_welcomeText",
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 20,
-                    fontWeight: FontWeight.w500,
-                  ),
-                )
-              ],
-            )
-          ],
-        ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: FlatButton(
+              child: Text('Signout'),
+              onPressed: () => _logout(),
+              color: Colors.redAccent,
+            ),
+          ),
+
+          Image.network(_imageUrl == null || _imageUrl.isEmpty ? "https://picsum.photos/250?image=9" : _imageUrl)
+        ],
       ),
+      )
+      
     );
+  }
+
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+  final GoogleSignIn _googleSignIn = GoogleSignIn();
+
+  Future<User> _gSigin() async {
+    GoogleSignInAccount googleSignInAccount = await _googleSignIn.signIn();
+    GoogleSignInAuthentication googleSignInAuthentication = await googleSignInAccount.authentication;
+
+    AuthCredential credential = GoogleAuthProvider.credential(
+        idToken: googleSignInAuthentication.idToken,
+        accessToken: googleSignInAuthentication.accessToken
+    );
+
+    UserCredential googleUserCredential = await _auth.signInWithCredential(credential);
+
+    User user = googleUserCredential.user;
+    print(user.displayName);
+    print(user.photoURL);
+
+    setState(() {
+      _imageUrl = user.photoURL;
+    });
+
+    return user;
+
+  }
+
+  _logout() {
+    setState(() {
+      _googleSignIn.signOut();
+      _imageUrl = null;
+    });
   }
 }
